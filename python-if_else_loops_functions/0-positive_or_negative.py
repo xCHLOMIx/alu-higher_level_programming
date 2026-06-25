24fc5d99c88b61d9ee39303c9464bd13dd550f68
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-24 23:16:52 -0700

    Change content of file 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 
 if number == 0:
-    print(f"{number:d} iz zero")
+    print(f"{number:d} is zero")
 elif number > 0:
     print(f"{number:d} is positive")
 elif number < 0:
