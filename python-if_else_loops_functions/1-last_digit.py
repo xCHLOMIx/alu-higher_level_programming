c41f30116e0e112f648a75bece9b7e31a0df3575
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-24 23:36:38 -0700

    Switch if outputs in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+number = -98
 
-if number >= 0:
+if number > 0:
     ld = int(str(number)[-1])
+elif number == 0:
+    ld = 0
 else:
     ld = int(str(number)[-1]) * -1 
 
 if ld > 5:
     print(f"Last digit of {number:d} is {ld:d} and is greater than 5")
 elif ld == 0:
-    print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
-else:
     print(f"Last digit of {number:d} is {ld:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {ld:d} and is less than 6 and not 0")
