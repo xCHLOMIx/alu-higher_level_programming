04bb650c4ad0399383f626f024c54e63a43bb307
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-24 23:33:37 -0700

    Add the negative infront of one in 1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     ld = int(str(number)[-1])
 else:
-    ld = int(str(number)[-1]) * 1 
+    ld = int(str(number)[-1]) * -1 
 
 if ld > 5:
     print(f"Last digit of {number:d} is {ld:d} and is greater than 5")
