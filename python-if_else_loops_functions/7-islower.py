aac4d2418dab4d0e5d1a8c3d97249566d9f5afbe
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 09:39:31 -0700

    Edit 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(letter):
-    if ord(letter) > 122 or ord(letter) < 97:
+    if ord(letter) < 122 or ord(letter) > 97:
         return True
     else:
         return False
