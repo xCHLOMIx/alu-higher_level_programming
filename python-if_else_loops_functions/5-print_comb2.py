8027a466d7475af6d8abffb59cf30fe39c4c6b90
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 07:31:35 -0700

    Edit content of 5-print_comb2.py file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i == 99:
-        print("{0:02d}".format(i), end="\n\n")
+        print("{0:02d}".format(i), end="\n")
         continue
     print("{0:02d}".format(i), end=", ")
