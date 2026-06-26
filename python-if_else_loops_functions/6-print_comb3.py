b3a5aafcb1e2d4cbe2d9381f7d7251c34c188739
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 08:28:05 -0700

    Add the file 6-print_comb3.py and make it executable

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i == j:
+            continue
+        if i < 5 and int(str(i) + str(j)) > int(str(j) + str(i)) and j + i == i + j:
+            continue
+        if i >= 5 and int(str(i) + str(j)) > int(str(j) + str(i)) and j + i == i + j:
+            continue
+        if i == 8 and j == 9:
+            print("{}{}".format(i,j), end="\n")
+            continue
+        print("{}{}".format(i,j), end=", ")
