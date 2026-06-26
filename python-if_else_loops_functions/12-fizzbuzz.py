d5303151f57efb248ee5c44a54ebb19f84cff200
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 10:39:55 -0700

    Edit file 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     final = ""
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0:
             final += "Fizz "
         elif i % 5 == 5:
