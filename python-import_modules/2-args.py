510c16a49f26387728bee8f8c93998011fb3b7ae
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 15:19:17 -0700

    Edit file 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,8 @@ import sys
 if __name__ == "__main__":
     args = sys.argv
 
-    if len(args) > 1:
-        print("{} arguments:".format(len(args) -1))
+    if len(args) - 1 > 1:
+        print("{} arguments:".format(len(args) - 1))
     else:
         print("{} argument:".format(len(args) - 1))
     for i in range(1, len(sys.argv)):
