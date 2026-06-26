53ce20e68504a9443b3dfc16b42336104b189839
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 10:35:54 -0700

    Add the 12-fizzbuzz.py and fix 11-pow.py files

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    final = ""
+    for i in range(1, 100):
+        if i % 3 == 0:
+            final += "Fizz "
+        elif i % 5 == 5:
+            final +=  "Buzz "
+        elif i % 3 == 0 and i % 5 == 0:
+            final += "FizzBuzz "
+        else:
+            final += "i "
