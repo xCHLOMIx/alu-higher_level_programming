ba2265bee7ca02e7f9af44b45349321ff9dfbb46
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 11:03:16 -0700

    Edit the file 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number > 0:
-        return str(number)[-1]
+        return str(number)[-1] + str(number)[-1]
     else:
-        return str((number * -1))[-1]
+        return str((number * -1))[-1] + str(number)[-1]
