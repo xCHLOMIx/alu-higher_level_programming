f7013829882a1f530f15b7d2077fab3e9ef8acec
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 08:52:01 -0700

    Edit file 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(letter):
-    if letter.islower():
+    if letter == '':
+        pass
+    elif letter.islower():
         return True
     else:
         return False
