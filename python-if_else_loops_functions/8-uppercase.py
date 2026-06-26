b96563814ee99c4d14f677f1fba9fcf86bcb8b13
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 10:05:34 -0700

    Add the file 8-uppercase.py and make it executable

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uppercase(str):
+    final = ""
+
+    for i in str.lower():
+        if i == " ":
+            final += i
+            continue
+        if ord(i) >= 48 and ord(i) <= 57:
+            final += i
+            continue
+        upper = ord(i) - 32
+        final += chr(upper)
+    return final
+print(uppercase("Best School 98 Battery street"))
