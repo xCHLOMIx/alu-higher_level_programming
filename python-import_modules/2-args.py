0ed366c36bfd5761f602b53d545c8611aa9c9354
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 15:04:10 -0700

    Edit the file 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     args = sys.argv
-    print("{} arguments:".format(len(args)))
+    print("{} arguments:".format(len(args) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
