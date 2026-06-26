1c1fb641ddccce1f7c2fea95f156f7d80df5d7eb
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 10:46:50 -0700

    Use the right condition order in 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 def fizzbuzz():
     final = ""
     for i in range(1, 101):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            final += "FizzBuzz "
+        elif i % 3 == 0:
             final += "Fizz "
         elif i % 5 == 0:
             final +=  "Buzz "
-        elif i % 3 == 0 and i % 5 == 0:
-            final += "FizzBuzz "
         else:
             final += f"{i} "
     return final
