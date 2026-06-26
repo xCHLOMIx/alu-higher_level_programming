1cf395f1735467459567d16c684aa87a77b66438
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-26 10:41:24 -0700

    Fix the modulus division condition 12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             final += "Fizz "
-        elif i % 5 == 5:
+        elif i % 5 == 0:
             final +=  "Buzz "
         elif i % 3 == 0 and i % 5 == 0:
             final += "FizzBuzz "
