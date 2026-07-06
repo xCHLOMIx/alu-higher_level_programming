53d3b4d913222ed6392e2c10ea4d36ed0fe03a70
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-06 07:32:56 -0700

    Add the file 4-new_in_list.py and make it executable

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    a = my_list
+    if idx < 0:
+        return a
+    if idx > len(my_list) - 1:
+        return a
+    else:
+        a[idx] = element
+        return a
