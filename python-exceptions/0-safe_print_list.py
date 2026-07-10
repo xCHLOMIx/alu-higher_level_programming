568d4ac3d979d64cce3b8c8ab41ca434a907d5ce
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 09:26:29 -0700

    Edit 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,11 +7,3 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     return int(a)
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
