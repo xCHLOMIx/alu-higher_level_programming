9c4467abc4d03675b2a69a5e4610439eccf3521a
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 10:03:15 -0700

    Add 2-safe_print_list_integers.py and make it executable

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    r = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            r += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        except IndexError:
+            pass
+    print("")
+    return r         
