7f40890f12059479d018e8d2758d840170a592a9
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 09:54:51 -0700

    Edit the file 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except TypeError, ValueError:
+    except TypeError:
        return False
+    except ValueError:
+        return False
