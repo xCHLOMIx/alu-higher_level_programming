06a070a6b8559109a71cebd323a542d90fbb91eb
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 10:23:47 -0700

    Edit the file 3-safe_print_division.py and make it executable

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
+    r = None
+    try:
+        r = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("{}".format(r))
+        return r
