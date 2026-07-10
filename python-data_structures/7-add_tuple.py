12931c30902d9a994f3e08e69d76710fa8c3a3c6
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 01:16:17 -0700

    Add the file 7-add_tuple.py and make it executable

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2:
-
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+        
     result = ((tuple_a[0] + tuple_b[1]), (tuple_a[1] + tuple_b[1]))
     return result
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
