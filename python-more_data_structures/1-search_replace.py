0d46f8dc717a9ef278e1364af80ef0eda019e777
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 02:22:45 -0700

    Add the file 1-search_replace.py and make it executable

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    def r(a):
+        if a == search:
+            return replace
+        else:
+            return a
+    return list(map(r, my_list))
+
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
