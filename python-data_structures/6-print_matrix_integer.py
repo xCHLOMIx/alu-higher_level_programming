644998f7864487c43e7700a6f1b3e3937ebd0eb5
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-09 23:46:44 -0700

    Edit content of 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -7,9 +7,3 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{:d}".format(i[j]), end=" ")
         print("")
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
