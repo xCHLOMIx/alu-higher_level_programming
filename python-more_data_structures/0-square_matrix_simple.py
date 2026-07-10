f14ba7b8b972c578299bae47b51febd8979cf596
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 02:14:49 -0700

    Edit the file 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     res = []
     for i in matrix:
-        res.append(list(map(lambda x: x ** 2, matrix[0])))
+        res.append(list(map(lambda x: x ** 2, i)))
     return res
