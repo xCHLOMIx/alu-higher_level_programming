218610e04145c5345df026d69fb0dab6dcc2e37a
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 10:15:51 -0700

    Edit 2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,9 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
             r += 1
         except TypeError:
             pass
-        except IndexError:
-            pass
         except ValueError:
+            pass
+        except IndexError:
             pass
             raise
     print("")
