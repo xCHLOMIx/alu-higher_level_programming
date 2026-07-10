b738c24dd96ffd8fe9e403272cae5716f747db71
xCHLOMIx <gutabarwaa@gmail.com>
2026-07-10 10:36:22 -0700

    Edit the file 4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
-            r.appendi
             print("division by 0")
         except IndexError:
             print("out of range")
